196da83ac822fa9f6469b6fb63ad29e1aeb9c924
Kustower1 <kmazurczak133@gmail.com>
2023-03-06 17:59:10 +0100

    Fresh project
    Default project with: Spring boot starter web, H2, JPA

---
+++ Section 1/SectionOne/src/test/java/com/example/SectionOne/SectionOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SectionOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SectionOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
